1dd77845706e9f9aab02da7c4855d0a4b79a083c
bryantwofourtwo <bryantwofourtwo@gmail.com>
2017-11-20 11:53:49 -0700

    added keys info

--- keys.js
+++ keys.js
@@ -1,10 +1,17 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'yYUt8ZBtH8962mY6M8qcg7FuE',
+  consumer_secret: 'JfswxSbffPproD5n6NHmK9cfP85iHv02TmwOTqoMRVoXJYSvsT',
+  access_token_key: '932342106437193728-MrrnTCzT1BZkAsdezmhRR08cVeYdGIj',
+  access_token_secret: 'myVP9silR9NnAu759gi7AGuUK3JmFsAk3D2FkD2o3eMxd',
 }
 
 module.exports = twitterKeys;
+
+var spotifyKeys = {
+  client_id: '0f9e5f6cdc7b4643a05ae8c2507185f4',
+  client_secret: '67fb2950ca9d4deea008e3a875dc9d5c'
+}
+
+module.exports = spotifyKeys;
